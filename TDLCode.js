08735f547c9716802d6c2466f6da86f26ba89ca5
Gabriel Custodio <gkustodor@gmail.com>
2021-05-16 15:38:57 -0300

    Finished a basic and funcional  to-do list.

---
+++ TDLCode.js
@@ -0,0 +1,48 @@
+function addTask () {
+    var input = document.getElementById("TextInput");
+    var newTask = input.value;
+    if (newTask != "") {
+      var item = document.createElement("li");
+      item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)" value="&#x2713;" /> ' + 
+      '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' +
+      newTask;
+      document.getElementById("tasks").appendChild(item);  
+      
+      input.value = "";
+      input.placeholder = "What else should you do?"
+    }
+  }
+  
+  
+  function markDone (item) { 
+      if (item.className == 'finished'){
+          item.className = 'li'
+      }
+      else{
+          item.className = 'finished'
+      }
+  }
+  
+  function remove (item) {
+        if (item.className == 'finished'){ 
+            item.remove();
+        }
+        else{
+            alert("You didn't mark this task as done.")
+        }
+  }
+  
+
+  function doAbout() {
+      var author = document.getElementById("divabout");
+      author.innerHTML = "Made by Gabriel Custodio.";
+      author.className = "yellowbackground";
+    
+    } 
+  
+  function clearAbout() {
+      var about = document.getElementById("divabout");
+      about.innerHTML = '<input type="button" onclick="doAbout()" value="About"></input>';
+      about.className = "normalbackground";
+  }
+  
